67aa26b903377e35e18d250a5cf77699c4dc57de
DashProk <dashkos-kokos@mail.ru>
2016-05-10 10:48:54 +0600

    First step

--- src/func.c
+++ src/func.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
-#include "func.h"
 
+void input(int field[], int N)
+{
+	N = 16;
+	int standart_field[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0};
+}
+
+int main()
+{
+	int N;
+	int field[N];
+}
