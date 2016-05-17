7041f1b3319d7281fd0fc0086d310454c8037aa9
DashProk <dashkos-kokos@mail.ru>
2016-05-17 10:06:35 +0600

    First work file

--- src/f2.c
+++ src/f2.c
@@ -2,31 +2,59 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main()
+void swap(int *a, int *b)
 {
-	int field[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void mixing(int *field, int n)
+{
 	int last = 15;
-	int i, k, temp;
+	int k;
 
 	srand(time(0));
 	while (last > 0)
 	{
-		k = rand()% 16;
-		temp = field[last];
-		field[last] = field[k];
-		field[k] = temp;
+		k = rand()% n;
+		swap(&field[k], &field[last]);
 		last--;
 	}
+}
 
-	for (i = 0; i < (16/2); i++)
+void filling(int *field, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
 	{
-		printf("%d\t", field[i]);
+		field[i] = i;
 	}
-	printf("\n");
-	for (i = (16/2); i < 16; i++)
+}
+
+void output(int *field, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
 	{
-		printf("%d\t", field[i]);
+		printf("%d ", field[i]);
 	}
+}
+
+
+
+int main()
+{
+	int n = 16;
+
+	int standart_field[n];
+	int work_field[n];
+
+	filling(standart_field, 16);
+	filling(work_field, 16);
+	mixing(work_field, 16);
+	output(work_field, 16);
 
 	return 0;
 }
